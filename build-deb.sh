b21496cffd30e8a64c21c1bf8f74159e2ff853db
Asadbek Sayfiev <61960169+BlackCatDevel0per@users.noreply.github.com>
2021-12-25 03:34:35 +0500

    update build-deb.sh

--- build-deb.sh
+++ build-deb.sh
@@ -58,7 +58,7 @@ cd $builddir
 # Set package directory.
 pkgdir=$(mktemp -d)/mcpelauncher-thesonicmaster
 # Ensure we are compiling with Clang.
-export CC=clang CXX=clang++
+export CC=gcc CXX=g++
 # Set compiler flags, but don't override existing flags.
 if [ -z "$CFLAGS" ]; then
   # Optimise for size (-Os)
